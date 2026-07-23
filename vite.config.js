2aa5f0130be145e9b7d66a6dab005d04345a5998
quan0790 <daviesquan8@gmail.com>
2026-07-23 11:40:32 +0300

    vercel

--- vite.config.js
+++ vite.config.js
@@ -19,21 +19,6 @@ export default defineConfig({
     },
   },
   build: {
-    chunkSizeWarningLimit: 1600,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
-              return 'vendor-react';
-            }
-            if (id.includes('framer-motion') || id.includes('lucide-react')) {
-              return 'vendor-ui';
-            }
-            return 'vendor';
-          }
-        }
-      }
-    }
+    chunkSizeWarningLimit: 2000,
   }
 });
