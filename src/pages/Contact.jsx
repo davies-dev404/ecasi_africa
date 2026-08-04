8abf68d0c228f5c2d6dae4bedf59c8cc6422e4b1
quan0790 <daviesquan8@gmail.com>
2026-08-04 23:02:32 +0300

    Website update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -58,7 +58,7 @@ const Contact = () => {
             className="flex justify-center"
           >
             <img
-              src="/images/team/image copy.png"
+              src="/images/team/image"
               alt="ECASI Africa Group photo"
               className="w-full max-w-[480px] h-auto object-cover rounded-2xl shadow-xl border border-slate-100"
             />
